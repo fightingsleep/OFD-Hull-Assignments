d92bdadc183d8b35ffc8b458a7eb4aad5f29851a
Chris Ross <chrislross@outlook.com>
2020-05-28 17:48:11 -0400

    Improve usability of forward rate calc a bit

--- Chapter4/ForwardRateCalculator/ForwardRateCalculator/ForwardRateCalculator.py
+++ Chapter4/ForwardRateCalculator/ForwardRateCalculator/ForwardRateCalculator.py
@@ -1,11 +1,12 @@
-terms = [1,2,3,4,5]
-zeroRates = [3, 4, 4.6, 5.0, 5.3]
+# Example input: [1,2,3,4,5]
+terms = eval(input('Input a list of terms: '))
+# Example input: [3,4,4.6,5,5.3]
+zeroRates = eval(input('Input a list of continuously compounded zero rates: '))
 
-zeroRate = input("Forward rate term:")
 forwardRateCurve = []
 
 for i in range(0,len(terms)):
     if i - 1 >= 0:
-        forwardRateCurve.append((zeroRates[i] * terms[i] - zeroRates[i-1] * terms[i-1]) / (terms[i] - terms[i-1]))
+        forwardRateCurve.append(round((zeroRates[i] * terms[i] - zeroRates[i-1] * terms[i-1]) / (terms[i] - terms[i-1]), 4))
 
-print(forwardRateCurve)
+print('Implied forward rates: ', forwardRateCurve)
